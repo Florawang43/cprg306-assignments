1a8b1ce4c429ba93292e5c0444c8120a85cb5133
Peter Sun <hailong.sun1982@gmail.com>
2024-09-11 14:59:59 -0600

    feat: init

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Link from "next/link";
+import StudentInfo from "./week-2/student-info";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="/week-2">week-2</Link>
+    </div>
+  );
+}
